8a4faa64908f6c794e1c30f69aaf330ac9be5de6
Benjamin Turner <benjamin@passionsplay.com>
2021-11-16 15:26:48 -0800

    feat: add basic form with file input

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,16 @@ import '../styles/main.css'
 function App() {
   return (
     <div className="mt-20">
-      <h1 className="font-bold text-4xl text-center text-gray-900">Hello, Electron!</h1>
-
-      <p className="p-2 text-center text-lg">I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-
-      <p className="p-2 text-center">Added <span className="font-bold text-blue-600">TailwindCSS</span> Support by <span className="font-bold text-blue-600" >Sanan Ali</span></p>
+      <h1 className="text-3xl text-center">
+        Nexo Income Tracker
+      </h1>
+      <form className="upload-csv p-4" action="">
+        <label for="nexo-csv-file" className="block p-2 mb-4 border">
+          <div>Upload CSV File</div>
+          <input id="nexo-csv-file" name="nexo-csv-file" type="file" className="hidden"/>
+        </label>
+        <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="submit">Process CSV</button>
+      </form>
     </div>
   )
 }
