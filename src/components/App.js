e1620c61465023d370225830b4a1657c0c6bd6b6
Benjamin Turner <benjamin@passionsplay.com>
2021-11-16 16:08:10 -0800

    feat: update form to make better use of useState()

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,27 @@ import React, { useState } from 'react'
 import '../styles/main.css'
 function App() {
 
-  let [csvFile, setCsvFile] = useState();
+  let [csvFile, setCsvFile] = useState({
+    name: '',
+    path: '',
+    mtime: '',
+    size: '',
+  });
 
   let handleChange = (e) => {
     e.preventDefault();
-    setCsvFile(e.target.value)
+    let file = e.target.files[0];
+    setCsvFile({
+      name: file.name,
+      path: file.path,
+      mtime: file.lastModified,
+      size: file.size,
+    })
+  }
+
+  let save = (e) => {
+    e.preventDefault();
+    console.log(csvFile)
   }
 
   return (
@@ -15,11 +31,12 @@ function App() {
       <h1 className="text-3xl text-center">
         Nexo Income Tracker
       </h1>
-      <form className="upload-csv p-4" action="">
-        <label for="nexo-csv-file" className="block p-2 mb-4 border">
+      <form className="upload-csv p-4" onSubmit={save}>
+        <label htmlFor="nexo-csv-file" className="block p-2 mb-4 border">
           <div>Upload CSV File</div>
-          <div>{csvFile}</div>
-          <input id="nexo-csv-file" name="nexo-csv-file" type="file" className="hidden" onChange={handleChange}/>
+          <div>{csvFile.name}</div>
+          <input id="nexo-csv-file" name="nexo-csv-file" type="file" className="hidden" onChange={handleChange}
+          />
         </label>
         <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="submit">Process CSV</button>
       </form>
