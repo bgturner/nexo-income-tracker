c42d67e5dfa2df051568b6ce06d3ca292dd8c032
Benjamin Turner <benjamin@passionsplay.com>
2021-11-19 21:02:27 -0800

    feat: add other tests related to the interest transaction

--- src/helpers/nexoCsv2Ledger.js
+++ src/helpers/nexoCsv2Ledger.js
@@ -1,11 +1,29 @@
 const hash = require("./hash");
 
+function getUsdAmount(amt) {
+  return parseFloat(amt.replace(/\$/g, ""));
+}
+
 function convertInterest(t) {
   return {
     date: t.date___time,
     transactionId: t.transaction,
     csvFingerprint: hash(t),
-    usdEquivalent: parseFloat(t.usd_equivalent.replace(/\$/g, "")),
+    usdEquivalent: getUsdAmount(t.usd_equivalent),
+    transactionType: 'Interest',
+    details: t.details,
+    entries: [
+      {
+        account: `Assets:Current Assets:Nexo:${t.currency}`,
+        currency: t.currency,
+        amount: t.amount,
+      },
+      {
+        account: `Income:Interest:Nexo:${t.currency}`,
+        currency: "USD",
+        amount: getUsdAmount(t.usd_equivalent),
+      },
+    ],
   };
 }
 module.exports = convertInterest;
--- test/helpers/nexoCsv2Ledger.test.js
+++ test/helpers/nexoCsv2Ledger.test.js
@@ -17,13 +17,20 @@ const nexoCsvReaderTransactions = {
   },
 };
 
+/**
+ * The desired state after transforming the interest transaction.
+ *
+ * Note that the csvFingerprint is a hash generated from the original
+ * transaction object so that we have a way of easily determining if
+ * that transaction has already been imported and transformed.
+ */
 const nexoJournalTransactions = {
   interest: {
     transactionId: "NXTcblPhKYupm",
     csvFingerprint:
       "b28c94b2195c8ed259f0b415aaee3f39b0b2920a4537611499fa044956917a21",
     date: "2021-11-12 01:00:03",
-    type: "Interest",
+    transactionType: "Interest",
     details: "approved / BTC Interest Earned",
     usdEquivalent: 1.8643086360035,
     entries: [
@@ -45,36 +52,91 @@ const convertedInterestTransaction = convertInterest(
   nexoCsvReaderTransactions.interest
 );
 
-test("The date___time property is converted to the date property.", () => {
-  expect(convertedInterestTransaction).toHaveProperty("date");
-  expect(convertedInterestTransaction).not.toHaveProperty("date___time");
-  expect(convertedInterestTransaction.date).toEqual(
-    nexoCsvReaderTransactions.interest.date___time
-  );
-});
-test.todo("The entries property has two entries."), () => {};
-test.todo("The entry for the income account has the correct values."), () => {};
-test.todo("The entry for the asset account has the correct values."), () => {};
-test("The transaction property is converted to the transactionId property.", () => {
-  expect(convertedInterestTransaction).toHaveProperty("transactionId");
-  expect(convertedInterestTransaction).not.toHaveProperty("transaction");
-  expect(convertedInterestTransaction.transactionId).toEqual(
-    nexoCsvReaderTransactions.interest.transaction
-  );
+describe("The main parts of the Income transaction are correct", () => {
+  test("The details property is correctly set.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("details");
+    expect(convertedInterestTransaction.details).toEqual(
+      nexoJournalTransactions.interest.details
+    );
+  });
+
+  test("The date___time property is converted to the date property.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("date");
+    expect(convertedInterestTransaction).not.toHaveProperty("date___time");
+    expect(convertedInterestTransaction.date).toEqual(
+      nexoCsvReaderTransactions.interest.date___time
+    );
+  });
+
+  test("The transaction property is converted to the transactionId property.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("transactionId");
+    expect(convertedInterestTransaction).not.toHaveProperty("transaction");
+    expect(convertedInterestTransaction.transactionId).toEqual(
+      nexoCsvReaderTransactions.interest.transaction
+    );
+  });
+
+  test("The csvFingerprint is correctly generated from the original object.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("csvFingerprint");
+    expect(convertedInterestTransaction.csvFingerprint).toEqual(
+      nexoJournalTransactions.interest.csvFingerprint
+    );
+  });
+
+  test("The usd_equivalent is converted to a float.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("usdEquivalent");
+    expect(convertedInterestTransaction).not.toHaveProperty("usd_equivalent");
+    expect(convertedInterestTransaction.usdEquivalent).toBeCloseTo(
+      nexoJournalTransactions.interest.usdEquivalent
+    );
+  });
+
+  test("The transactionType property is correctly set.", () => {
+    expect(convertedInterestTransaction).toHaveProperty("transactionType");
+    expect(convertedInterestTransaction.transactionType).toBe("Interest");
+  });
 });
 
-test("The csvFingerprint is correctly generated from the original object.", () => {
-  expect(convertedInterestTransaction).toHaveProperty("csvFingerprint");
-  expect(convertedInterestTransaction.csvFingerprint).toEqual(
-    "b28c94b2195c8ed259f0b415aaee3f39b0b2920a4537611499fa044956917a21"
-  );
-});
+describe("The transaction entries are generated correctly.", () => {
+  test("The entries property has at least two entries.", () => {
+    expect(convertedInterestTransaction.entries.length).toBeGreaterThanOrEqual(
+      2
+    );
+  });
 
-test("The usd_equivalent is converted to a float.", () => {
-  expect(convertedInterestTransaction).toHaveProperty("usdEquivalent");
-  expect(convertedInterestTransaction).not.toHaveProperty("usd_equivalent");
-  expect(convertedInterestTransaction.usdEquivalent).toBeCloseTo(
-    nexoJournalTransactions.interest.usdEquivalent
-  );
-});
+  describe("The Asset entry has the correct values.", () => {
+    test("The amount property of the Asset entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[0].amount).toBeCloseTo(
+        nexoJournalTransactions.interest.entries[0].amount
+      );
+    });
+    test("The currency property of the Asset entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[0].currency).toBe(
+        nexoJournalTransactions.interest.entries[0].currency
+      );
+    });
+    test("The account property of the Asset entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[0].account).toBe(
+        nexoJournalTransactions.interest.entries[0].account
+      );
+    });
+  });
 
+  describe("The Income entry has the correct values.", () => {
+    test("The amount property of the Income entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[1].amount).toBeCloseTo(
+        nexoJournalTransactions.interest.entries[1].amount
+      );
+    });
+    test("The currency property of the Income entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[1].currency).toBe(
+        nexoJournalTransactions.interest.entries[1].currency
+      );
+    });
+    test("The account property of the Income entry is correct.", () => {
+      expect(convertedInterestTransaction.entries[1].account).toBe(
+        nexoJournalTransactions.interest.entries[1].account
+      );
+    });
+  });
+});
